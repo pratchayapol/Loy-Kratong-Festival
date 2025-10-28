b8d0d0c427eade225bf70617d6aa7688d5af4bf3
pratchayapol <pratchayapol@github.com>
2025-10-28 14:08:00 +0700

    เ

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KrathongController;
 
 Route::get('/', [KrathongController::class, 'show'])->name('krathong.show');
-Route::post('/krathongs', [KrathongController::class,'store'])->name('krathongs.store');
+// บันทึกกระทง (Ajax)
+Route::post('/krathongs', [KrathongController::class, 'store'])->name('krathongs.store');
+
+// กันกรณีมีใครเผลอกดไปที่ /krathongs แบบ GET
+Route::get('/krathongs', fn() => redirect('/'))->name('krathongs.redirect');
