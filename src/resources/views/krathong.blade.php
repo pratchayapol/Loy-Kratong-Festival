96347da04e1acc18ef7095e0dd03be94128dbff5
pratchayapol <pratchayapol@github.com>
2025-10-28 16:58:33 +0700

    ด

--- src/resources/views/krathong.blade.php
+++ src/resources/views/krathong.blade.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Loy Kratong Festival</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Favicon (Laravel asset) -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <!-- Favicon -->
+    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" sizes="any" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
     <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     <meta name="theme-color" content="#0b2e4a">
     <!-- Tailwind & Alpine -->
