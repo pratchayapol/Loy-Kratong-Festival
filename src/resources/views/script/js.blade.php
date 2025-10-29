0cff310a0accd1519c30436993aa9e9dfaa6fd66
pratchayapol <pratchayapol@github.com>
2025-10-29 23:35:04 +0700

    fd

--- src/resources/views/script/js.blade.php
+++ src/resources/views/script/js.blade.php
@@ -326,7 +326,7 @@
         const MAX_ITEMS = window.innerWidth < 640 ? 15 : 35;
         const typeImg = t => types?.[t]?.img || Object.values(types || {})[0]?.img || '';
 
-        const TOTAL_LANES = 2;
+        const TOTAL_LANES = 3;
         const TOP_PAD = window.innerWidth < 640 ? 6 : 4;
         const BOTTOM_PAD = window.innerWidth < 640 ? 6 : 4;
 
