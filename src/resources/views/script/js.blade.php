d87dcca68d5660f84d46f744bc1c6d65e2af0d12
pratchayapol <pratchayapol@github.com>
2025-10-29 23:10:01 +0700

    ด

--- src/resources/views/script/js.blade.php
+++ src/resources/views/script/js.blade.php
@@ -330,7 +330,7 @@
         const TOTAL_LANES = window.innerWidth < 640 ? 1 : 2;
 
         // เพิ่มระยะขอบบน–ล่างของแถวนานๆ เข้าไปเลย
-        const TOP_PAD = window.innerWidth < 640 ? 6 : 8; // ยิ่งเยอะ ยิ่งห่างขอบบน
+        const TOP_PAD = window.innerWidth < 640 ? 44 : 42; // ยิ่งเยอะ ยิ่งห่างขอบบน
         const BOTTOM_PAD = window.innerWidth < 640 ? 24 : 22; // ยิ่งเยอะ ยิ่งห่างขอบล่าง
 
         // คำนวณพื้นที่ที่เหลือให้วางเลน
