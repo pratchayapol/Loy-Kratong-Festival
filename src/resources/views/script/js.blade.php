9880e17fdd3201ce58670b8ab2f6e8a808537a40
pratchayapol <pratchayapol@github.com>
2025-10-29 23:04:34 +0700

    เด

--- src/resources/views/script/js.blade.php
+++ src/resources/views/script/js.blade.php
@@ -339,7 +339,7 @@
         const TRACK_WIDTH = 140;
         const KRATHONG_WIDTH_PCT = window.innerWidth < 640 ? 40 : 15;
         const SAFE_GAP_PCT = KRATHONG_WIDTH_PCT * 2; // เว้นระยะ 2 เท่า!
-        const TIME_GAP_MS = (SAFE_GAP_PCT / TRACK_WIDTH) * DUR * 1000;
+        const TIME_GAP_MS = (SAFE_GAP_PCT / TRACK_WIDTH) * DUR * 1300;
 
         console.log(
             `Config: ${TOTAL_LANES} lanes, vertical: ${VERTICAL_SPACING.toFixed(1)}%, gap: ${(TIME_GAP_MS/1000).toFixed(1)}s`
