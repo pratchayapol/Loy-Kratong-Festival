798d4051df29dcf3e7cb130e49d675f8156ba812
pratchayapol <pratchayapol@github.com>
2025-10-29 13:14:48 +0700

    อ

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,9 +10,8 @@ Route::post('/krathongs', [KrathongController::class,'store'])->name('krathongs.
 // กันเผลอ GET
 Route::match(['get','head'], '/krathongs', fn() => abort(405));
 
-Route::get('/kuma/heartbeat/{slug}', function ($slug) {
+Route::get('/kuma/heartbeat/{slug}', function (string $slug) {
     $r = Http::timeout(10)->get("https://kuma.pcnone.com/api/status-page/heartbeat/{$slug}");
     abort_unless($r->ok(), $r->status(), $r->body());
-    return response($r->body(), 200)
-        ->header('Content-Type', 'application/json');
+    return response($r->body(), 200)->header('Content-Type', 'application/json');
 });
